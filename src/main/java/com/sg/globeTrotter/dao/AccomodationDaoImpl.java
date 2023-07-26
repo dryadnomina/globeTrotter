a437795d4e27bfba94aa1c5aeb8236b8dc65851d
Maryann Dodi <67160994+dryadnomina@users.noreply.github.com>
2023-07-25 23:47:32 -0400

    Add implementation to AccomodationDaoImpl

--- src/main/java/com/sg/globeTrotter/dao/AccomodationDaoImpl.java
+++ src/main/java/com/sg/globeTrotter/dao/AccomodationDaoImpl.java
@@ -5,37 +5,73 @@
 package com.sg.globeTrotter.dao;
 
 import com.sg.globeTrotter.dto.Accomodation;
+import com.sg.globeTrotter.dto.Trip;
+import com.sg.globeTrotter.mappers.AccomodationMapper;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  *
  * @author marya
  */
-public class AccomodationDaoImpl implements AccomodationDao{
+public class AccomodationDaoImpl implements AccomodationDao {
+
+    @Autowired
+    JdbcTemplate jdbc;
 
     @Override
     public Accomodation getAccomodationByID(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+
+            String sql = "SELECT * FROM accomodation WHERE accomodationId = ?";
+            return jdbc.queryForObject(sql, new AccomodationMapper(), id);
+        } catch (DataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
     public List<Accomodation> getAllAccomodations() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "SELECT * FROM accomodation;";
+        return jdbc.query(sql, new AccomodationMapper());
     }
 
     @Override
     public Accomodation addAccomodation(Accomodation accomodation) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        final String sql = "INSERT INTO accomodation (name, type,description,tripId) "
+                + "VALUES(?,?,?)";
+
+        Trip trip = accomodation.getTrip();
+
+        jdbc.update(sql,
+                accomodation.getName(),
+                accomodation.getType(),
+                accomodation.getDescription(),
+                trip.getId(),
+                accomodation.getId()
+        );
+
+        int newId = jdbc.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
+        accomodation.setId(newId);
+        return accomodation;
     }
 
     @Override
     public void updateAccomodation(Accomodation accomodation) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "UPDATE accomodation SET name = ?, type = ?, description = ? WHERE accomodationId = ?";
+        jdbc.update(sql,
+                accomodation.getName(),
+                accomodation.getType(),
+                accomodation.getDescription(),
+                accomodation.getId());
     }
 
     @Override
     public void deleteAccomodationByID(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "DELETE FROM accomodation WHERE accomodationId = ?";
+        jdbc.update(sql, id);
     }
-    
+
 }
