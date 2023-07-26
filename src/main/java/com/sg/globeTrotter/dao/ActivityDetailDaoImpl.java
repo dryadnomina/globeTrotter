72e18a75620511361c91cecf550d8e295062f790
Maryann Dodi <67160994+dryadnomina@users.noreply.github.com>
2023-07-25 22:02:20 -0400

    Implement code in ActivityDetailDaoImpl

--- src/main/java/com/sg/globeTrotter/dao/ActivityDetailDaoImpl.java
+++ src/main/java/com/sg/globeTrotter/dao/ActivityDetailDaoImpl.java
@@ -5,7 +5,11 @@
 package com.sg.globeTrotter.dao;
 
 import com.sg.globeTrotter.dto.ActivityDetail;
+import com.sg.globeTrotter.mappers.ActivityDetailMapper;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  *
@@ -13,29 +17,55 @@ import java.util.List;
  */
 public class ActivityDetailDaoImpl implements ActivityDetailDao {
 
+    @Autowired
+    JdbcTemplate jdbc;
+
     @Override
     public ActivityDetail getActivityDetailByID(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        try {
+
+            String sql = "SELECT * FROM detail WHERE detailId = ?";
+            return jdbc.queryForObject(sql, new ActivityDetailMapper(), id);
+        } catch (DataAccessException ex) {
+            return null;
+        }
     }
 
     @Override
     public List<ActivityDetail> getAllActivityDetails() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "SELECT * FROM detail;";
+        return jdbc.query(sql, new ActivityDetailMapper());
     }
 
     @Override
     public ActivityDetail addActivityDetail(ActivityDetail detail) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        final String sql = "INSERT INTO detail (address,cost,description) "
+                + "VALUES(?,?,?,?,?)";
+
+        jdbc.update(sql,
+                detail.getAddress(),
+                detail.getCost(),
+                detail.getDescription()
+        );
+
+        int newId = jdbc.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
+        detail.setId(newId);
+        return detail;
     }
 
     @Override
     public void updateActivityDetail(ActivityDetail detail) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "UPDATE detail SET address = ?,description = ?,cost = ? WHERE detailId = ?";
+        jdbc.update(sql, detail.getAddress(),
+                detail.getDescription(),
+                detail.getCost(),
+                detail.getId());
     }
 
     @Override
     public void deleteActivityDetailByID(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String sql = "DELETE FROM detail WHERE detailId = ?";
+        jdbc.update(sql, id);
     }
-    
+
 }
