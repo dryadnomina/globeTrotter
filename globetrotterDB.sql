084670b6e0f18b2d359aa1c05532646bc1b1fd96
Maryann Dodi <67160994+dryadnomina@users.noreply.github.com>
2023-07-31 10:52:53 -0400

    Fix issue with addTravellerToTrip page

--- globetrotterDB.sql
+++ globetrotterDB.sql
@@ -4,79 +4,81 @@ CREATE DATABASE globetrotterdb;
 
 USE globetrotterdb;
 
+ 
   CREATE TABLE IF NOT EXISTS traveller (
 	travellerId INT NOT NULL AUTO_INCREMENT,
-	tripId INT NOT NULL,
 	firstName  VARCHAR(50) NOT NULL,
 	lastName  VARCHAR(50) NOT NULL,
-	poastalCode CHAR(6) NULL,
-	city  VARCHAR(50) NULL,
-    phoneNumber CHAR(10) NULL,
+	postalCode CHAR(6),
+	city  VARCHAR(50),
+    phoneNumber CHAR(10) NOT NULL,
 	PRIMARY KEY pk_travellerId (travellerId) 
 );
 
 CREATE TABLE IF NOT EXISTS trip (
 	tripId INT NOT NULL AUTO_INCREMENT,
-	title VARCHAR(50) NOT NULL,
-	type VARCHAR(15) NOT NULL,
-	description VARCHAR(255) NOT NULL,
+	title VARCHAR(100) NOT NULL,
+	type VARCHAR(30) NOT NULL,
+	description VARCHAR(255),
+    startDate DATE NOT NULL,
+    endDate Date NOT NULL,
     PRIMARY KEY pk_tripId (tripId)
 );
 
 CREATE TABLE IF NOT EXISTS accomodation (
 	accomodationId INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(50) NOT NULL,
-    type VARCHAR(15) NOT NULL,
+	name VARCHAR(100) NOT NULL,
+    type VARCHAR(20) NOT NULL,
 	description VARCHAR(255) NOT NULL,
     tripId INT NOT NULL,
 	PRIMARY KEY pk_accomodationId (accomodationId)
 );
 
+CREATE TABLE IF NOT EXISTS budget (
+		budgetId  INT NOT NULL AUTO_INCREMENT,
+        	name VARCHAR(100) NOT NULL,
+    accomodationCost Decimal(7,2) NOT NULL,
+    foodCost DECIMAL(7,2) NOT NULL,
+    activityCost DECIMAL(7,2) NOT NULL,
+    transportationCost DECIMAL(7,2) NOT NULL,
+    total DECIMAL(10,2) NOT NULL,
+    tripId INT NOT NULL,
+
+	PRIMARY KEY pk_budgetId (budgetId)
+);
+
 
 CREATE TABLE IF NOT EXISTS tripTraveller (
-	travellerId INT NOT NULL,
-	tripId INT NOT NULL,
+	travellerId INT ,
+	tripId INT,
 	PRIMARY KEY pk_tripTraveller (tripId, travellerId)
 );
 
 
 CREATE TABLE IF NOT EXISTS activity (
 	activityId INT NOT NULL AUTO_INCREMENT,
-    tripId INT NOT NULL,
     activityName VARCHAR(100) NOT NULL,
+    description VARCHAR(255),
+	address VARCHAR(150) NOT NULL,   
+    tripId INT NOT NULL,
   	PRIMARY KEY pk_activityId (activityId)
   );
   
-CREATE TABLE IF NOT EXISTS detail (
-	detailId INT NOT NULL AUTO_INCREMENT,
-	description VARCHAR(255) NULL,
-	address VARCHAR(150) NOT NULL,
-    cost DECIMAL NOT NULL,
-    activityId INT NOT NULL,
-	PRIMARY KEY pk_detailId (detailId)   
-  );
 
--- FK
-
-ALTER TABLE detail
-	ADD CONSTRAINT fk_activityId
-    FOREIGN KEY fk_activityId (activityId)
-    REFERENCES activity (activityId);
+ALTER TABLE accomodation
+ADD CONSTRAINT fk_tripId_a
+  FOREIGN KEY fk_tripId_a (tripId)
+   REFERENCES trip (tripId);
     
     ALTER TABLE activity
 	ADD CONSTRAINT fk_tripId
     FOREIGN KEY fk_tripId (tripId)
     REFERENCES trip (tripId);
   
-ALTER TABLE trip
-	ADD CONSTRAINT fk_accomodationId
-    FOREIGN KEY fk_accomodationId (tripId)
-    REFERENCES accomodation (accomodationId);
-    
-ALTER TABLE trip
-	ADD CONSTRAINT fk_travellerId
-    FOREIGN KEY fk_travellerId (travellerId)
-    REFERENCES traveller (travellerId);
+   ALTER TABLE budget
+	ADD CONSTRAINT fk_tripId_b
+    FOREIGN KEY fk_tripId_b (tripId)
+    REFERENCES trip (tripId);
     
 ALTER TABLE tripTraveller
 	ADD CONSTRAINT fk_tripTravellerTrip
